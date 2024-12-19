b7332ef8b92a194220b9d534d7ba58370f2ed709
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2024-12-19 17:56:31 +0900

    Add configuration types for prompts and settings in conf_types.py, and update .gitignore to include _types.py

---
+++ conf/_types.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import List, Tuple, Dict, Union, Optional, TypedDict
+
+
+class TextPrompt(TypedDict):
+    prompt: str
+    keyframes: Dict[str, float]
+
+
+class ImagePrompt(TypedDict):
+    image_path: str
+    keyframes: Dict[str, float]
+
+
+@dataclass
+class Config:
+    max_frames: int
+    text_prompts: List[TextPrompt]
+    width: int
+    height: int
+    model: str
+    save_all_iterations: bool = False
+    seed: Optional[int] = 0
+    interval: int = 1
+    initial_image: Optional[str] = None
+    target_images: List[ImagePrompt] = []
+    angle: Optional[Dict[str, float]] = None
+    zoom: Optional[Dict[str, float]] = None
+    translation_x: Optional[Dict[str, float]] = None
+    translation_y: Optional[Dict[str, float]] = None
+    iterations_per_frame: Optional[Dict[str, int]] = None
